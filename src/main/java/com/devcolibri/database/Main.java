658d666a18d1415011a5bf1580173d0ecae9b2c6
Egor Yarmolenko <egor.w869@gmail.com>
2018-08-29 01:25:52 +0300

    ResultSet

--- src/main/java/com/devcolibri/database/Main.java
+++ src/main/java/com/devcolibri/database/Main.java
@@ -3,79 +3,66 @@ package com.devcolibri.database;
 
 
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.*;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
-   /* public static final String URL = "jdbc:mysql://localhost:3306/mydbtest?autoReconnect=true&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    public static final String URL = "jdbc:mysql://localhost:3306/mydbtest?autoReconnect=true&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
-*/
+
+    private static final String INSERT_NEW = "INSERT INTO dish VALUES(?, ?, ?, ?, ?, ?, ?)";
+    private static final String GET_ALL = "SELECT * FROM dish";
+    private static final String DELETE = "DELETE FROM dish WHERE id = ?";
+
+
     public static void main(String[] args) {
 
-        DBWorker dbWorker = new DBWorker();
 
-        String qerry = "select * from users_tb where id = 2";
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
         try {
-            Statement statement = dbWorker.getConnection().createStatement();
-            ResultSet resultSet = statement.executeQuery(qerry);
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            preparedStatement = connection.prepareStatement(DELETE);
+            preparedStatement.setInt(1,2);
+            preparedStatement.executeUpdate();
+            preparedStatement = connection.prepareStatement(GET_ALL);
+            ResultSet result = preparedStatement.executeQuery();
 
-            while (resultSet.next()){
-                User user = new User();
-                user.setId(resultSet.getInt("id"));
-                user.setUsername(resultSet.getString("username"));
-                user.setPassword(resultSet.getString("password"));
+            while (result.next()){
+                int id = result.getInt("id");
+                String title = result.getString("title");
+                String description = result.getString("description");
+                float rating = result.getFloat("rating");
+                boolean published = result.getBoolean("published");
+                Date date = result.getDate("created");
+                byte[] icon = result.getBytes("icon");
 
-                System.out.println(user);
-               /* int id = resultSet.getInt(1);
-                System.out.println(id);*/
+                System.out.println("id: " + id + ", title: " + title + ", description: " + description +
+                ", rating: " + rating + ", published: " + published + ", created: " + date + ", icon length: " +
+                icon.length);
             }
-        } catch (SQLException e) {
+            /*preparedStatement.setInt(1, 2);
+            preparedStatement.setString(2, "Inerted title");
+            preparedStatement.setString(3, "Inserted desc");
+            preparedStatement.setFloat(4, 0.2f);
+            preparedStatement.setBoolean(5, true);
+            preparedStatement.setDate(6, new Date(Calendar.getInstance().getTimeInMillis()));
+
+            preparedStatement.setBlob(7, new FileInputStream("Smile.png"));
+
+            preparedStatement.execute();*/
+
+        }
+        catch(SQLException e){
             e.printStackTrace();
         }
-
-
-
-
-
-       /* Connection connection = null;
-        Driver driver;
-
-        try   {
-            driver = new com.mysql.cj.jdbc.Driver();
-            DriverManager.registerDriver(driver);
-        }
-        catch (SQLException e1) {
-            System.out.println("Драйвер не зарегистрировался");
-            return;
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            if (!connection.isClosed())
-                System.out.println("Соединение установлено");
-            Statement statement = connection.createStatement();
-            statement.addBatch("INSERT INTO animal(anim_name, anim_desc) VALUES ('batch1', 'desc');");
-            statement.addBatch("INSERT INTO animal(anim_name, anim_desc) VALUES ('batch2', 'desc');");
-            statement.addBatch("INSERT INTO animal(anim_name, anim_desc) VALUES ('batch3', 'desc');");
-
-            statement.executeBatch();
-
-            statement.clearBatch();
-            statement.getConnection();
-
-        }catch (SQLException ex){
-            System.err.println("Соединение не установлено");
-            ex.printStackTrace(); // Понадобился, чтобы отловить исключения, скрытые выводом на экран предупреждения
-            return;
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                    System.out.println("Соединение с БД закрыто");
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }*/
     }
 }
